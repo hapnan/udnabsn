3390045c80a7543fef065bf2cfb8a9a0e3c5a73b
HapnanAR <34102619+hapnan@users.noreply.github.com>
2024-05-25 23:47:51 +0700

    add Vercer SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <TRPCReactProvider>
-            <SpeedInsights />
             {children}
+            <SpeedInsights />
             <Toaster />
           </TRPCReactProvider>
         </ThemeProvider>
